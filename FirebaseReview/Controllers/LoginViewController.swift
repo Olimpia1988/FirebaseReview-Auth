440134e16e85827e5e07ea49b12212e9f8cacb84
Olimpia <olimpiacompean@prusuit.org>
2019-11-14 16:15:57 -0500

    working on video

--- FirebaseReview/Controllers/LoginViewController.swift
+++ FirebaseReview/Controllers/LoginViewController.swift
@@ -1,10 +1,11 @@
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
    
-    @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var emailUserTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var errorLabel: UILabel!
     
@@ -17,13 +18,26 @@ class LoginViewController: UIViewController {
     
     func initialSetup() {
         errorLabel.isHidden = true
-        Utilities.styleTextField(nameTextField)
-        Utilities.styleTextField(emailTextField)
+        Utilities.styleTextField(emailUserTextField)
+        Utilities.styleTextField(passwordTextField)
         Utilities.styleFilledButton(loginButton)
         
     }
 
     @IBAction func loginAction(_ sender: Any) {
+        let email = emailUserTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result , error) in
+            if error != nil {
+                self.errorLabel.isHidden = false
+                self.errorLabel.text = error!.localizedDescription
+            } else {
+                let homeVC = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.storyBoardID) as? HomeViewController
+                self.view.window?.rootViewController = homeVC
+                self.view.window?.makeKeyAndVisible()
+            }
+        }
     }
     
 
--- FirebaseReview/Controllers/ViewController.swift
+++ FirebaseReview/Controllers/ViewController.swift
@@ -1,11 +1,14 @@
 import UIKit
 import FirebaseAuth
+import AVFoundation
 
 
 
 class ViewController: UIViewController {
     
     //MARK: Properties
+    var videoLayer: AVPlayerLayer?
+    var videoPlayer: AVPlayer?
   
     
 
@@ -18,6 +21,11 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         initialSetup()
        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        setUpVideo()
     }
     
     @IBAction func signupAction(_ sender: Any) {
@@ -30,6 +38,28 @@ class ViewController: UIViewController {
         Utilities.styleFilledButton(singUpButton)
         Utilities.styleHollowButton(loginButton)
         
+    }
+    
+    func setUpVideo() {
+        //get the path to the resouce in the bundle
+       let path = Bundle.main.path(forResource: "login", ofType: "mp4")
+        guard path != nil else {
+            return
+        }
+        let url = URL(fileURLWithPath: path!)
+        
+        //create the video player item
+        let item = AVPlayerItem(url: url)
+        
+        //Create the player
+        videoPlayer = AVPlayer(playerItem: item)
+        
+        //Create the layer
+        videoLayer = AVPlayerLayer(player: videoPlayer!)
+        
+        //Adjust the size of the frame
+        videoLayer?.frame = CGRect(x: -self.view.frame.size.width*1.5, y: 0, width: self.view.frame.size.width*4, height: self.view.frame.height)
+        view.layer.insertSublayer(videoLayer!, at: 0)
     }
 
 
